6dd9c4a2c069a78e9a98fa5ecb009201fcc00481
JacobLau1 <%jacoblau05@gmail.com%>
2021-11-28 14:46:30 -0500

    I added all the buttons regarding the level selection screen, and not sure but think I deleted the other Undead Tales file with a dash inbetween. Also added the level selection screen itself.

---
+++ Undead Tales/LevelSelection.java
@@ -0,0 +1,38 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class LevelSelection here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LevelSelection extends World
+{
+
+    /**
+     * Constructor for objects of class LevelSelection.
+     * 
+     */
+    public LevelSelection()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(960, 540, 1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Level1 level1 = new Level1();
+        addObject(level1,303,99);
+        level1.setLocation(343,99);
+        Level2 level2 = new Level2();
+        addObject(level2,121,361);
+        Level3 level3 = new Level3();
+        addObject(level3,745,465);
+        level1.setLocation(300,113);
+    }
+}
